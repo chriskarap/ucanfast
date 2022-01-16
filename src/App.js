59778a3ee54645c75e1e7120970820371f41da92
chriskarap <karapetyanchristina@gmail.com>
2022-01-16 09:52:45 +0400

    start app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [position, setPosition] = useState();
+
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      //console.log(navigator.geolocation.getCurrentPosition());
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("chka");
+    }
+  }
+
+  function showPosition(position) {
+    console.log("ka");
+
+    setPosition({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          {position && position.coords ? (
+            <>
+              <span>{`Latitude: ${position.coords.latitude}`} </span>
+              <span>{`Longitude: ${position.coords.longitude}`} </span>
+            </>
+          ) : (
+            <span>Waiting ...</span>
+          )}
+        </div>
       </header>
     </div>
   );
